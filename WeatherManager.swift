490b410f7a1f8348d34416bce3e6cf901de72fc6
MistyBui <44589957+MistyBui@users.noreply.github.com>
2023-10-23 15:35:06 +0300

    Update WeatherManager.swift

--- WeatherManager.swift
+++ WeatherManager.swift
@@ -15,7 +15,7 @@ protocol WeatherManagerDelegate {
 }
 
 struct WeatherManager {
-    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=7d06259c43d3ca0fb420e371901ff2be&units=metric"
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=<appId>&units=metric"
     
     var delegate: WeatherManagerDelegate?
     
